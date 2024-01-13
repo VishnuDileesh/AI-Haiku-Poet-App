51ea12cda17c7ec123b0bd9a5a575e21f2470c81
VishnuDileesh <munnadileesh@gmail.com>
2024-01-13 18:59:20 +0530

    Add - frontend for ai haikupoet with expressjs and htmx

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
+import bodyParser from 'body-parser';
 import 'dotenv/config';
 import express from 'express';
+import fetch from 'node-fetch';
 import path from 'path';
 
 
@@ -8,10 +10,24 @@ const __dirname = path.resolve();
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+
+app.post('/get-haiku', async (req, res) => {
+    await fetch(`https://ai-haiku-poet-zt6iuojbma-uc.a.run.app/haiku?theme=${req.body.theme}`)
+        .then(res => res.json())
+        .then(data => res.send(`
+            <article>
+                <h3 style="white-space: pre-line;">${data['Haiku']}</h3>
+            </article>
+        `))
+        .catch(err => console.log(err));
+});
 
 const port = parseInt(process.env.PORT) || 3000;
 
